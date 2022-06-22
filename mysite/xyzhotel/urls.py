db0edb564e00f5d56fd6748ce109a76c8f9b61a6
lyndatcd <lyndatcd@users.noreply.github.com>
2022-06-22 12:02:12 +0100

    Update urls.py

--- mysite/xyzhotel/urls.py
+++ mysite/xyzhotel/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns=[
     path('',views.index,name='index'),
     path('book',views.book,name='book'),
+    path('book_now',views.book_now,name='book_now'),
     path('contact-us',views.contact,name='contact-us'),
 ]
